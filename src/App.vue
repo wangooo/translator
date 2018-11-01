f426158d599e1eacc6c08f9e204869c9c2737e30
wangoo <32057255+wangooo@users.noreply.github.com>
2018-11-01 15:50:53 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -22,12 +22,25 @@ export default {
     TranslateForm,TranslateOutput
   },
   methods: {
-    submitTextFun:function(text){
-      // alert(text);
-      this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20181031T075947Z.a9ae76141b0a1408.f7f730c924f3a93b3a4849a6ea99cd13bf78cdfd&lang=en&text='+text)
-          .then((response)=>{
-            this.reloutPut=response.body.text[0];
-          })
+    submitTextFun:function(text,val){
+      if(val=="en"){
+          this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20181031T075947Z.a9ae76141b0a1408.f7f730c924f3a93b3a4849a6ea99cd13bf78cdfd&lang=en&text='+text)
+              .then((response)=>{
+                this.reloutPut=response.body.text[0];
+              })
+      }
+      else if(val=="japenese"){
+          this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20181031T075947Z.a9ae76141b0a1408.f7f730c924f3a93b3a4849a6ea99cd13bf78cdfd&lang=ja&text='+text)
+              .then((response)=>{
+                this.reloutPut=response.body.text[0];
+              })
+      }
+      else{
+          this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20181031T075947Z.a9ae76141b0a1408.f7f730c924f3a93b3a4849a6ea99cd13bf78cdfd&lang=ko&text='+text)
+              .then((response)=>{
+                this.reloutPut=response.body.text[0];
+              })
+      }
     }
   }
 }
